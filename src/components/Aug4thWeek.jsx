b9d9e1ad2264c23780b5902978c3598b927d3c6e
SeoSangtae001027 <zmfmzl@naver.com>
2023-08-06 00:05:34 +0900

    [Feat] #4 - 단어장뷰끝

--- src/components/Aug4thWeek.jsx
+++ src/components/Aug4thWeek.jsx
@@ -4,56 +4,72 @@ import ProfileIcon from '../assets/Profile.svg';
 import BookmarkIcon from './BookmarkIcon';
 
 const contentData = [
-  {
-    name: '사과',
-    pronunciation: '[sagua]',
-    type: 'noun - Apple',
-  },
-  {
-    name: '사과',
-    pronunciation: '[sagua]',
-    type: 'noun - Apple',
-  },
-  {
-    name: '사과',
-    pronunciation: '[sagua]',
-    type: 'noun - Apple',
-  }
+    {
+        name: '사과',
+        pronunciation: '[sagua]',
+        type: 'noun - Apple',
+    },
+    {
+        name: '사과',
+        pronunciation: '[sagua]',
+        type: 'noun - Apple',
+    },
+    {
+        name: '사과',
+        pronunciation: '[sagua]',
+        type: 'noun - Apple',
+    },
 ];
 
 const Aug4thWeek = () => {
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="company-name">
-          Koreasy
-          <img src={ProfileIcon} alt="Profile" className="profile-icon" />
-        </div>
-        <div className="separator"></div>
-      </div>
-      <div className="content">
-        {contentData.map((content, index) => (
-          <div key={index} className="item" style={{ top: `${index * 135}px` }}>
-            <div className="item-info">
-              <div className="item-name">
-                {content.name}<span className="pronunciation">{content.pronunciation}</span>
-              </div>
-              <div className="item-type">
-                {content.type}
-                <BookmarkIcon
-                  onClick={(isClicked) => {
-                    console.log('Bookmark Icon Clicked:', isClicked);
-                  }}
-                />
-              </div>
-              <div className="item-example">Example sentences</div>
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="company-name">
+                    Koreasy
+                    <img
+                        src={ProfileIcon}
+                        alt="Profile"
+                        className="profile-icon"
+                    />
+                </div>
+                <div className="separator" />
             </div>
-            <div className="separator"></div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="content">
+                {contentData.map((content, index) => (
+                    <div
+                        key={index}
+                        className="item"
+                        style={{ top: `${index * 135}px` }}
+                    >
+                        <div className="item-info">
+                            <div className="item-name">
+                                {content.name}
+                                <span className="pronunciation">
+                                    {content.pronunciation}
+                                </span>
+                            </div>
+                            <div className="item-type">
+                                {content.type}
+                                <BookmarkIcon
+                                    onClick={isClicked => {
+                                        console.log(
+                                            'Bookmark Icon Clicked:',
+                                            isClicked,
+                                        );
+                                    }}
+                                />
+                            </div>
+                            <div className="item-example">
+                                Example sentences
+                            </div>
+                        </div>
+                        <div className="separator" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Aug4thWeek;
--- src/components/Wordbook.jsx
+++ src/components/Wordbook.jsx
@@ -1,52 +1,85 @@
 import React from 'react';
 import './Wordbook.scss';
-import backIcon from '../assets/back.svg';
-import searchIcon from '../assets/Search.svg';
-import frontIcon from '../assets/front.svg';
+import ProfileIcon from '../assets/Profile.svg';
+import TabBarItem from '../assets/TabBarItem.svg';
+import Line from '../assets/Line.svg';
+
+/**
+ * @typedef {Object} ContentDataItem
+ * @property {string} dateText - Date text.
+ * @property {string} dateRange - Date range.
+ */
+
+/**
+ * @type {ContentDataItem[]}
+ */
 
 const contentData = [
-  {
-    dateText: '4th week, Aug',
-    dateRange: '2023.08.21 ~ 08.25',
-  },
-  {
-    dateText: '3rd week, Aug',
-    dateRange: '2023.08.14 ~ 08.18',
-  },
-  {
-    dateText: '2nd week, Aug',
-    dateRange: '2023.08.07 ~ 08.11',
-  },
-  {
-    dateText: '1st week, Aug',
-    dateRange: '2023.07.31 ~ 08.04',
-  },
-  {
-    dateText: '4th week, Aug',
-    dateRange: '2023.07.24 ~ 07.28',
-  }
+    {
+        dateText: '4th week, Aug',
+        dateRange: '2023.08.21 ~ 08.25',
+    },
+    {
+        dateText: '3rd week, Aug',
+        dateRange: '2023.08.14 ~ 08.18',
+    },
+    {
+        dateText: '2nd week, Aug',
+        dateRange: '2023.08.07 ~ 08.11',
+    },
+    {
+        dateText: '1st week, Aug',
+        dateRange: '2023.07.31 ~ 08.04',
+    },
+    {
+        dateText: '4th week, Jul',
+        dateRange: '2023.07.24 ~ 07.28',
+    },
 ];
 
+/**
+ * @returns {JSX.Element}
+ */
+
 const Wordbook = () => {
-  return (
-    <div className="wordbook-container">
-      <div className="header">
-        <img className="icon-back" src={backIcon} alt="Back" />
-        <span className="title">Wordbook</span>
-        <img className="icon-search" src={searchIcon} alt="Search" />
-      </div>
-      <div className="date-section">
-        {contentData.map((content, index) => (
-          <div key={index} className="date-item" style={{ top: `${index * 110}px` }}>
-            <span className="date-text">{content.dateText}</span>
-            <div className="separator"></div>
-            <span className="date-range">{content.dateRange}</span>
-            <img className="icon-front" src={frontIcon} alt="Front" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="WordbookContainer">
+            <div className="WordbookHeader">
+                <div className="ServiceName">
+                    Koreasy
+                    <img
+                        src={ProfileIcon}
+                        alt="Profile"
+                        className="ProfileIcon"
+                    />
+                </div>
+            </div>
+            <div className="Space" />
+            <div className="PageTitle">ViewName</div>
+            <div className="DateSection">
+                {contentData.map((content, index) => (
+                    <div key={index} className="DateItemContainer">
+                        <div className="DateItem">
+                            <div className="DateInfo">
+                                <span className="DateText">
+                                    {content.dateText}
+                                </span>
+                                <span className="DateRange">
+                                    {content.dateRange}
+                                </span>
+                            </div>
+                            <img
+                                className="TabBarItem"
+                                src={TabBarItem}
+                                alt="Front"
+                            />
+                        </div>
+                        <img className="Line" src={Line} alt="Line" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Wordbook;
