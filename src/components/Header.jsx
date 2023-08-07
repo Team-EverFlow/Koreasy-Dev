1da5ca3a1bc048f7bfdcc8a2ba0b7a3e7419c90d
Mule129 <a24cc410s@naver.com>
2023-08-07 14:54:37 +0900

    [Feat] #10 - add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.scss';
+import ProfileIconDump from '../assets/Profile_Red.svg';
+import PreviousView from './NavigationBar';
+import Divider from './Divider';
+import PageTitle from './PageTitle';
+
+const Header = () => {
+    const ProfileIcon = ProfileIconDump;
+    return (
+        <div>
+            <div className="header">
+                <div className="service-name">
+                    Koreasy
+                    <img
+                        src={ProfileIcon}
+                        alt="Profile"
+                        className="profile-icon"
+                    />
+                </div>
+            </div>
+            <PreviousView />
+            <Divider />
+        </div>
+    );
+};
+
+export default Header;
