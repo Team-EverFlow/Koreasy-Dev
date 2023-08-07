4b4f077aeff7cd325861e88b89a67b4442faea9d
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 00:30:57 +0900

    [Feat] #15 - Add Header to badge

--- src/pages/MyBadges.jsx
+++ src/pages/MyBadges.jsx
@@ -1,10 +1,13 @@
 import { React } from 'react';
-
+import Header from '../components/Header';
 
 function MyBadgesView() {
     return (
         <div>
+            <Header></Header>
             <h1> Badges</h1>
         </div>
-    )
+    );
 }
+
+export default MyBadgesView;
