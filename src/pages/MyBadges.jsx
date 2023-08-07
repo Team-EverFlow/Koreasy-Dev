ab10ffb6d2dc0ca62071abbdfaf6ce8450c2642c
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 02:37:22 +0900

    [Feat] #15 - Work in progress (MyBadge)

--- src/pages/MyBadges.jsx
+++ src/pages/MyBadges.jsx
@@ -1,12 +1,23 @@
 import { React } from 'react';
+import '../styles/MyBadge.scss';
 import Header from '../components/Header';
+import Badge from '../components/Badge';
 
 function MyBadgesView() {
     return (
         <div>
             <Header isNavigationBar={true} viewName={'My Badges'} />
-            <div>
-
+            <div className={'badge-group'}>
+                <Badge imageUrl={'https://yhs.kr/static/image/python.svg'} />
+                <Badge
+                    imageUrl={'https://yhs.kr/static/image/python.svg'}
+                    active={true}
+                />
+                <Badge
+                    imageUrl={'https://yhs.kr/static/image/python.svg'}
+                    active={true}
+                />
+                <Badge imageUrl={'https://yhs.kr/static/image/python.svg'} />
             </div>
         </div>
     );
