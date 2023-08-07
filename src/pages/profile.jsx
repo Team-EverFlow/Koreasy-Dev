588e91cfbf08a1db589b1ba44190c10a690bd2c4
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 02:43:46 +0900

    [Feat] #14 - profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,1 +1,13 @@
 import { React } from "react";
+import Header from '../components/Header';
+
+
+function ProfileView() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default ProfileView;
