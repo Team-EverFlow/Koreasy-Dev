080581661704306d21f88740b8d2ff3deb9458df
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 00:42:05 +0900

    [Style] #32 - Using simple operator

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import Chevron from './Chevrion';
 import './NavigationBar.scss';
 
-const NavigationBar = ({ value }) => {
+const NavigationBar = ({ View }) => {
     // 컴포넌트에서 인자 전달시 사용
     /**
-     * @View {string || undefined}
+     * @View {string | undefined}
      */
-    let View = value;
-    if (View === undefined) {
-        View = 'PreviousView';
-    }
     return (
         <div className="previous-view">
             <div className="text">
                 <Chevron direction="Left" color="MainColor" />
-                {View}
+                {View === undefined ? 'PreviousView' : View}
             </div>
             <div className="dump-box" />
         </div>
