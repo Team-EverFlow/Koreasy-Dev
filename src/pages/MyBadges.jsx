78260ae89507e45976c04eb6d4484b8a526f186a
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 16:45:55 +0900

    [Feat] #15 - Add Dump Data (MyBadge)

--- src/pages/MyBadges.jsx
+++ src/pages/MyBadges.jsx
@@ -1,23 +1,28 @@
 import { React } from 'react';
 import Header from '../components/Header';
-import Badge from '../components/Badge';
+import BadgeGroup from '../components/BadgeGroup';
 
 function MyBadgesView() {
+    let dumpBadge = [
+        {
+            id: 1,
+            title: 'Python 최고',
+            imageUrl: 'https://yhs.kr/static/image/python.svg',
+            date: new Date(),
+            active: false,
+        },
+        {
+            id: 2,
+            title: 'Kotlin 최고',
+            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
+            date: new Date(),
+            active: true,
+        },
+    ];
     return (
         <div>
             <Header isNavigationBar={true} viewName={'My Badges'} />
-            <div className={'badge-group'}>
-                <Badge imageUrl={'https://yhs.kr/static/image/python.svg'} />
-                <Badge
-                    imageUrl={'https://yhs.kr/static/image/python.svg'}
-                    active={true}
-                />
-                <Badge
-                    imageUrl={'https://yhs.kr/static/image/python.svg'}
-                    active={true}
-                />
-                <Badge imageUrl={'https://yhs.kr/static/image/python.svg'} />
-            </div>
+            <BadgeGroup badges={dumpBadge} />
             <div></div>
         </div>
     );
