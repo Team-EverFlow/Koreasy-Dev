19a3cb6e4cbd4d63dd840ed34ef40370c7a27708
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 17:17:32 +0900

    [Docs] #14 - Add ProfileObject at typedef

--- src/types/typedef.js
+++ src/types/typedef.js
@@ -7,3 +7,12 @@
  * @property {Date} date
  *
  */
+
+/**
+ * @typedef {Object} ProfileObject
+ * @property {string} id
+ * @property {string} name
+ * @property {string} profileIcon
+ * @property {Array<BadgeObject>} badges
+ *
+ */
