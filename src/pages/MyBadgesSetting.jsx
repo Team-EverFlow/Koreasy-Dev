3df3811f3a5df2748c9ddf9ccd4a7559a68b5158
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 16:22:04 +0900

    [Feat] #15 - Add MyBadgesSetting Page (WIP)

--- src/pages/MyBadgesSetting.jsx
+++ src/pages/MyBadgesSetting.jsx
@@ -2,7 +2,7 @@ import { React } from 'react';
 import Header from '../components/Header';
 import Badge from '../components/Badge';
 
-function MyBadgesView() {
+function MyBadgesViewSetting() {
     return (
         <div>
             <Header isNavigationBar={true} viewName={'My Badges'} />
