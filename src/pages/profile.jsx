fafbf43d1707c27505e197eb9f5a0dc92a8117ae
gunyu1019 <gunyu1019@gmail.com>
2023-08-08 13:32:02 +0900

    [Feat] #14 - profile image and nickname

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,11 +1,20 @@
-import { React } from "react";
+import React from 'react';
 import Header from '../components/Header';
-
+import ProfileIconDump from '../assets/images/ProfileRed.svg';
+import '../styles/profile.scss';
 
 function ProfileView() {
+    const ProfileIcon = ProfileIconDump;
     return (
         <div>
             <Header />
+            <div className={'profile'}>
+                <div className={'profile-background'}></div>
+                <div className={'profile-main'}>
+                    <img src={ProfileIcon} />
+                    <span className={'profile-nickname'}>Halogen</span>
+                </div>
+            </div>
         </div>
     );
 }
