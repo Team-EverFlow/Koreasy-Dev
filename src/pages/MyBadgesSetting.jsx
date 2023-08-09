bbc52d0decaeecb88720b603f92ca172e5aa06de
gunyu1019 <gunyu1019@gmail.com>
2023-08-10 03:26:06 +0900

    [Feat] #45 - Use state for check badge

--- src/pages/MyBadgesSetting.jsx
+++ src/pages/MyBadgesSetting.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import BadgeGroup from '../components/BadgeGroup';
 import '../styles/MyBadge.scss';
+import dumpBadge from './dumpBadge';
 
 function MyBadgesViewSetting() {
+    let [badge, setBadge] = useState(dumpBadge);
     return (
         <div>
             <Header isNavigationBar={true} viewName="My Badges" />
-            <BadgeGroup badges={[]} />
+            <BadgeGroup
+                badges={badge}
+                onClick={id => {
+                    let newBadge = [...badge];
+                    console.log(id);
+                    newBadge[id].check = !newBadge[id].check;
+                    setBadge(newBadge);
+                }}
+            />
             <div className="my-badge-button-group">
                 <button className="my-badge-button cancel">Cancel</button>
                 <button className="my-badge-button">Confirm</button>
