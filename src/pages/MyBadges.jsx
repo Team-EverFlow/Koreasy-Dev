e6d7b52f19844b4e970ca288370f49762be1703e
gunyu1019 <gunyu1019@gmail.com>
2023-08-10 03:24:48 +0900

    [Feat] #45 - Add dumpBadge data

--- src/pages/MyBadges.jsx
+++ src/pages/MyBadges.jsx
@@ -2,66 +2,9 @@ import React from 'react';
 import Header from '../components/Header';
 import BadgeGroup from '../components/BadgeGroup';
 import '../styles/MyBadge.scss';
+import dumpBadge from './dumpBadge';
 
 function MyBadgesView() {
-    let dumpBadge = [
-        {
-            id: 1,
-            title: 'Python 최고',
-            imageUrl: 'https://yhs.kr/static/image/python.svg',
-            date: new Date(),
-            active: false,
-            check: false,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: true,
-            check: false,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: false,
-            check: true,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: true,
-            check: false,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: true,
-            check: true,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: true,
-            check: false,
-        },
-        {
-            id: 2,
-            title: 'Kotlin 최고',
-            imageUrl: 'https://yhs.kr/static/image/kotlin.svg',
-            date: new Date(),
-            active: true,
-            check: false,
-        },
-    ];
     return (
         <div>
             <Header isNavigationBar={true} viewName="My Badges" />
