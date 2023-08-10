5aeb1d53ba34bafa9e045058e1df2ae45e6ceaa4
gunyu1019 <gunyu1019@gmail.com>
2023-08-11 02:12:56 +0900

    [Feat] #53 - Add onClick handler

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import '../styles/Login.scss';
 import { Button } from 'react-bootstrap';
 import googleLogo from '../assets/images/google.png';
+import { GoogleAuth } from '../firebase/api/authAPI';
 
 function LoginPage() {
+    const onLoginButtonClick = () => {
+        GoogleAuth().then(result => {
+            console.log(result.state, result.user);
+            switch (result.state) {
+                case 'register':
+                    return;
+                case 'error':
+                    return;
+                case 'signIn':
+                    return;
+            }
+        });
+    };
+
     return (
         <div>
             <div className={'login-service-group'}>
@@ -13,7 +28,7 @@ function LoginPage() {
                 </span>
             </div>
             <div className={'login-button-group'}>
-                <Button className={'login-button'}>
+                <Button className={'login-button'} onClick={onLoginButtonClick}>
                     <img src={googleLogo} alt={'google logo'} />
                     <span className={'login-button-title'}>
                         Sign in with Google
