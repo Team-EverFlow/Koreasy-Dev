3c5ffe02ba31ebcdf076ad6fb1488397a01b3bca
gunyu1019 <gunyu1019@gmail.com>
2023-08-11 01:36:03 +0900

    [Feat] #44 - Set Route with Profile, MyBadges and MyBadgesSetting

--- src/Router.js
+++ src/Router.js
@@ -2,13 +2,19 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainView from './pages/mainView/MainPage';
+import MainView from './pages/MyBadgesSetting';
+import Profile from './pages/Profile';
+import MyBadges from './pages/MyBadges';
+import MyBadgesSetting from './pages/MyBadgesSetting';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainView />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/badge" element={<MyBadges />} />
+                <Route path="/badge/setting" element={<MyBadgesSetting />} />
             </Routes>
         </BrowserRouter>
     );
