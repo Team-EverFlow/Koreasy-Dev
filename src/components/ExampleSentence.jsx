1afae8b9d41a9e51cb99d5fcf53fca0194b5094e
Mule129 <a24cc410s@naver.com>
2023-08-11 02:40:25 +0900

    [Docs] #49 - Add JSDoc

--- src/components/ExampleSentence.jsx
+++ src/components/ExampleSentence.jsx
@@ -3,26 +3,37 @@ import './ExampleSentence.scss';
 import Sound from '../assets/images/Sound.svg';
 
 /**
- *
- * @param {{boolean}} isSound
- * @param {{string}} exampleText
- * @param {{string}} mean
- * @returns
+ * @typedef {Json} exampleSentence
+ * @property {string} sentenceKr
+ * @property {string} sentenceEn
  */
-function ExampleSentence({ isSound, exampleText, mean }) {
+
+/**
+ * test
+ * @param {{isSound: boolean, exampleSentence: {sentenceKr: string, sentenceKr: string} }} exampleText
+ * @returns {React.ReactElement}
+ */
+function ExampleSentence({ isSound, exampleText }) {
     return (
-        <div className="background">
-            {isSound === true ? (
+        <div className="exsentence-background">
+            {isSound && (
                 <div className="sound">
                     <div className="sound-box">
                         <img src={Sound} alt="sound" />
                     </div>
                 </div>
-            ) : null}
-            <div>{exampleText}</div>
-            <div>{mean}</div>
+            )}
+            <div>{exampleText.sentenceKr}</div>
+            <div>{exampleText.sentenceEn}</div>
         </div>
     );
 }
 
 export default ExampleSentence;
+/**
+ * 설명
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {}
