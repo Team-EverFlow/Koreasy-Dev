a655da4ea43ce6651db054bc06c26afa84c149c0
gunyu1019 <gunyu1019@gmail.com>
2023-08-10 15:57:45 +0900

    [Feat] #45 - Divide onClick event to onBadgeClick Handler

--- src/pages/MyBadgesSetting.jsx
+++ src/pages/MyBadgesSetting.jsx
@@ -6,18 +6,21 @@ import dumpBadge from './dumpBadge';
 
 function MyBadgesViewSetting() {
     let [badge, setBadge] = useState(dumpBadge);
+
+    const onBadgeClick = id => {
+        if (!badge[id].active) {
+            setBadge(prevBadge => {
+                return prevBadge.map((b, index) => {
+                    return index === id ? { ...b, check: !b.check } : b;
+                });
+            });
+        }
+    };
+
     return (
         <div>
             <Header isNavigationBar={true} viewName="My Badges" />
-            <BadgeGroup
-                badges={badge}
-                onClick={id => {
-                    let newBadge = [...badge];
-                    console.log(id);
-                    newBadge[id].check = !newBadge[id].check;
-                    setBadge(newBadge);
-                }}
-            />
+            <BadgeGroup badges={badge} onClick={onBadgeClick} />
             <div className="my-badge-button-group">
                 <button className="my-badge-button cancel">Cancel</button>
                 <button className="my-badge-button">Confirm</button>
