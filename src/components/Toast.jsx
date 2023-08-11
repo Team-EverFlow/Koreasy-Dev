104c2c358956371ed9789257d023c271cac01f7c
gunyu1019 <gunyu1019@gmail.com>
2023-08-11 21:33:26 +0900

    [Feat] #61 - Toast design and add support multi-line message

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,10 +1,20 @@
 import './Toast.scss';
 
-export function Toast({ reference, message, icon = undefined }) {
+export function Toast({ reference, message, icon = false }) {
     return (
-        <div ref={reference} className="toast-message">
-            {icon !== undefined && <span className={'toast-icon' + icon} />}
-            {message}
+        <div ref={reference} className="toast-group">
+            <div className={`toast-content ${icon && 'toast-icon-content'}`}>
+                {icon && <span className="toast-icon" />}
+                <div className="toast-message">
+                    {message.split('\\n').map(message_content => {
+                        return (
+                            <span className="toast-message-block">
+                                {message_content}
+                            </span>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
