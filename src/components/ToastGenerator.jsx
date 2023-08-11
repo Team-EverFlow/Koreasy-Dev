0a5c4b3e95630e48fe40240e694a8b333ca9b379
gunyu1019 <gunyu1019@gmail.com>
2023-08-11 17:28:46 +0900

    [Fix] #61 - Miss send reference variable to toast component

--- src/components/ToastGenerator.jsx
+++ src/components/ToastGenerator.jsx
@@ -3,12 +3,19 @@ import { Toast } from './Toast';
 
 export function ToastGenerator() {
     const reference = useRef(null);
-    const onCall = () => {
+    const onToastCallEvent = () => {
         reference.current.classList.add('show');
         setTimeout(function () {
             reference.current.classList.remove('show');
         }, 3000);
     };
 
-    return [Toast, onCall];
+    return [
+        ({ message, icon = undefined }) => {
+            return (
+                <Toast reference={reference} icon={icon} message={message} />
+            );
+        },
+        onToastCallEvent,
+    ];
 }
