2d0f557fe10fbc68f607dd01c85d13d5fcaa3354
gunyu1019 <gunyu1019@gmail.com>
2023-08-13 16:15:16 +0900

    [Feat] - #67 recovering login oauth2 feature

--- src/Router.js
+++ src/Router.js
@@ -6,11 +6,26 @@ import MainView from './pages/MyBadgesSetting';
 import Profile from './pages/Profile';
 import MyBadges from './pages/MyBadges';
 import MyBadgesSetting from './pages/MyBadgesSetting';
+import Login from './pages/Login';
+import WordTestList from './pages/wordTest/WordTestList';
+import TestPageGoToTest from './pages/wordTest/TestPageGoToTest';
+import TestPageLearnWords from './pages/wordTest/TestPageLearnWords';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<WordTestList />} />
+                <Route
+                    path="/TestPageGoToTest"
+                    element={<TestPageGoToTest />}
+                />
+                <Route
+                    path="/TestPageLearnWords"
+                    element={<TestPageLearnWords />}
+                />
+
                 <Route path="/" element={<MainView />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/badge" element={<MyBadges />} />
