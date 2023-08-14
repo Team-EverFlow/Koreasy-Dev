59baf38db1adf9a2d559fbb50d0de89da927d5ac
Mule129 <a24cc410s@naver.com>
2023-08-14 22:01:50 +0900

    Revert "[Feat] #35 - Add auti fit background cord width"
    This reverts commit 049fd278a1550b0bcbea34b65145ca158f579290.

--- src/pages/todayWordPage/TodayWordView.jsx
+++ src/pages/todayWordPage/TodayWordView.jsx
@@ -1,66 +1,19 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ReactCardFlip from 'react-card-flip';
-
+import React from 'react';
 import TodayWordCard from './TodayWordCard.jsx';
 import Header from '../../components/Header.jsx';
 import TodayCheckComponent from './TodayCheckComponent.jsx';
-import backCardImg from '../../assets/images/Thumbnail1.png';
-
-import WordData from '../../utils/dumpFile/todayWordDump.js';
 
 import '../../styles/todayWordPage/TodayWordView.scss';
 
 function TodayWordView() {
-    const [wordCardHeight, setWordCardHeight] = useState(200);
-    const [isCheckText, setIsCheckText] = useState({
-        first: false,
-        second: false,
-        third: false,
-        fourth: false,
-    });
-
-    let wordCard = useRef(null);
-
-    useEffect(() => {
-        if (wordCard.current) {
-            const wordCardHeight = wordCard.current.scrollHeight;
-            setWordCardHeight(wordCardHeight);
-        }
-    }, []);
-
-    const handleClick = cnt => {
-        setIsCheckText(isCheckText => ({
-            ...isCheckText,
-            [cnt]: true,
-        }));
-    };
-
     return (
-        <div className="todaywordview">
+        <div className="TodayWordView">
             <Header />
-            {Object.keys(isCheckText).map((item, index) => (
-                <ReactCardFlip
-                    isFlipped={isCheckText[item]}
-                    flipDirection="horizontal"
-                    className="card-box"
-                    key={index}
-                >
-                    <div
-                        className="todayword-back-img"
-                        style={{ height: wordCardHeight + 'px' }}
-                    >
-                        <img
-                            src={backCardImg}
-                            alt="backcard"
-                            onClick={() => handleClick(item)}
-                        />
-                    </div>
-                    <div ref={wordCard} className="wordcard-height">
-                        <TodayWordCard wordData={WordData[index]} />
-                    </div>
-                </ReactCardFlip>
-            ))}
-            <TodayCheckComponent checkText={isCheckText} />
+            <div className="card-box">
+                <TodayWordCard />
+                <TodayWordCard />
+                <TodayCheckComponent />
+            </div>
         </div>
     );
 }
