a4ccab8407615b913505c2e7d4b9925de5a455a6
gunyu1019 <gunyu1019@gmail.com>
2023-08-14 19:13:04 +0900

    [Feat] #72 - Change Type BadgeObject

--- src/types/typedef.js
+++ src/types/typedef.js
@@ -2,7 +2,7 @@
  * @typedef {Object} BadgeObject
  * @property {number} id
  * @property {string} title
- * @property {string} imageUrl
+ * @property {string} image
  * @property {boolean} active
  * @property {boolean} [check=false] check
  * @property {Date} date
