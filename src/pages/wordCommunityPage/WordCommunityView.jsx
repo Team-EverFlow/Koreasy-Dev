880686109890067f20172c58cd207310403aaab5
Mule129 <a24cc410s@naver.com>
2023-08-14 22:01:19 +0900

    [Feat] #35 - Add WordCommunity View

--- src/pages/wordCommunityPage/WordCommunityView.jsx
+++ src/pages/wordCommunityPage/WordCommunityView.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Comment from './Comment';
 import AddComment from './AddComment';
 import WordCardText from '../../components/WordCardText';
 import Header from '../../components/Header';
-import wordData from './wordData';
+
+import wordCommentDump from '../../utils/dumpFile/wordCommentDump';
 
 import '../../styles/wordCommunityPage/WordCommunityView.scss';
 
 function WordCommunityView() {
-    const data = wordData;
+    const [wordComment, setWordComment] = useState({ ...wordCommentDump });
+
+    function reload() {
+        // setWordComment();
+        // featch server data
+    }
+
     return (
         <>
-            <Header />
+            <Header isNavigationBar={true} navigationViewName="사과" />
             <div className="community-background">
                 <div className="community-word-info">
                     <WordCardText />
                 </div>
 
                 <div className="community-comment">
-                    <Comment />
-                    <Comment />
+                    {Object.keys(wordComment.wordCommentList).map(
+                        (_, index) => (
+                            <Comment
+                                key={index}
+                                wordComment={wordComment.wordCommentList[index]}
+                            />
+                        ),
+                    )}
                 </div>
 
                 <AddComment />
