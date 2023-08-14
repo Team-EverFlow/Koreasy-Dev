d7bcf4064fe01c633f75ae292791952e93a31d3d
Mule129 <a24cc410s@naver.com>
2023-08-14 20:52:51 +0900

    [Feat] #35 - Add TodayCheckComponent

--- src/pages/todayWordPage/TodayCheckComponent.jsx
+++ src/pages/todayWordPage/TodayCheckComponent.jsx
@@ -1,17 +1,70 @@
 import React from 'react';
+
+import TodayEvtEN from '../../utils/TodayCvtEN';
+
 import '../../styles/todayWordPage/TodayCheckComponent.scss';
 
-function TodayCheckComponent({ cnt }) {
+/**
+ *
+ * @param {{checkText: <Array>}} checkText
+ * @returns
+ */
+function TodayCheckComponent({ checkText }) {
+    const checkTextStatus = { ...checkText };
+
+    const countTrueValues = Object.values(checkTextStatus).reduce(
+        (count, value) => {
+            if (value === false) {
+                return count + 1;
+            }
+            return count;
+        },
+        0,
+    );
+
+    let cnt = countTrueValues;
+    const allValuesTrue = Object.values(checkTextStatus).every(
+        value => value === true,
+    );
+    let message = allValuesTrue
+        ? {
+              needWord: '🎉 You attended!',
+              chierUp: TodayEvtEN(),
+          }
+        : {
+              needWord: 'Check ' + cnt + ' more words',
+              chierUp: 'for attenddance',
+          };
+
+    function sendTodayCheckData() {
+        //
+    }
+    if (allValuesTrue) {
+        sendTodayCheckData();
+    }
+
     return (
         <div>
             <div className="today-check-background">
                 <div className="circle-frame">
-                    <div className="circle-first">1</div>
-                    <div className="circle-first">2</div>
+                    {Object.keys(checkTextStatus).map((item, index) => (
+                        <div
+                            key={index}
+                            className={
+                                checkTextStatus[item]
+                                    ? 'check-word'
+                                    : 'not-check-word'
+                            }
+                        >
+                            {index + 1}
+                        </div>
+                    ))}
                 </div>
                 <div className="today-check-text-frame">
-                    <span className="today-check-title">you attended</span>
-                    <span className="today-check-info">aug 5, 2023</span>
+                    <span className="today-check-title">
+                        {message.needWord}
+                    </span>
+                    <span className="today-check-info">{message.chierUp}</span>
                 </div>
             </div>
         </div>
