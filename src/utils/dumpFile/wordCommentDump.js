963b799628240dd6b799197aa9ea3ef6f03af23b
Mule129 <a24cc410s@naver.com>
2023-08-14 20:50:48 +0900

    [Docs] #35 - Add dump file

---
+++ src/utils/dumpFile/wordCommentDump.js
@@ -0,0 +1,28 @@
+const wordCommunity = {
+    wordId: 'adf',
+    wordCommentList: [
+        {
+            userName: 'testUserName',
+            date: '2023.08.14',
+            commentID: 'abcd',
+            commentInfo: '토마토 먹고싶다',
+            heartCnt: 3,
+        },
+        {
+            userName: 'testUserName',
+            date: '2023.08.14',
+            commentID: 'abcd',
+            commentInfo: '스프 먹고싶다',
+            heartCnt: 2,
+        },
+        {
+            userName: 'testUserName',
+            date: '2023.08.14',
+            commentID: 'abcd',
+            commentInfo: '당근 먹고싶다',
+            heartCnt: 0,
+        },
+    ],
+};
+
+export default wordCommunity;
