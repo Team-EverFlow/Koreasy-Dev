59c827bc237029b22e75b538ae682ff5558179b1
gunyu1019 <gunyu1019@gmail.com>
2023-08-16 19:52:24 +0900

    [Feat] #70 - Update method 'AddRepBadge' to 'UpdateRepBadge'

--- src/firebase/api/BadgeApi.js
+++ src/firebase/api/BadgeApi.js
@@ -4,11 +4,11 @@ import { USER_COLLECTION_ID } from '../type/const';
 import { GetCurrentUserFromFirebase } from './userAPI';
 
 /**
- * 해당 뱃지id를 현재 유저의 UserInformation repBadge에 추가합니다.
- * @param {string} badgeId
+ * 해당 뱃지 id를 현재 유저의 UserInformation repBadge 를 설정합니다.
+ * @param {Array<string>} badgeId
  * @returns {{ success: boolean, error: any | undefined }}
  */
-export async function AddRepBadge(badgeId) {
+export async function UpdateRepBadge(badgeId) {
     try {
         const userInfoRef = doc(
             db,
