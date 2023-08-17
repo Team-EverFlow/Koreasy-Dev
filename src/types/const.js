6d045d817aeca2555793e08af4df9b74c4c4e470
gunyu1019 <gunyu1019@gmail.com>
2023-08-18 03:19:53 +0900

    [Feat] #134 - AddDispatcher at backend :D

---
+++ src/types/const.js
@@ -0,0 +1,3 @@
+export const REPLIES_ACHIEVEMENT_EVENT_NAME = 'replies_achievement';
+export const TEST_RESULT_ACHIEVEMENT_EVENT_NAME = 'test_result_achievement';
+export const ATTENDANCE_ACHIEVEMENT_EVENT_NAME = 'attendance_achievement';
