169b8e8aa0b5c12cce6c82c0e178eb077ff30d8b
gunyu1019 <gunyu1019@gmail.com>
2023-08-18 03:19:12 +0900

    [Feat] #134 - AddEventListener to BadgeNotificationGenerator

--- src/components/BadgeNotification.jsx
+++ src/components/BadgeNotification.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import '../styles/components/BadgeNotification.scss';
 import BadgeList from '../pages/mybadge/badgeList';
 import { badgeImage } from '../utils/badgeImage';
