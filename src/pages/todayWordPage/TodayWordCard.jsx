63d4c1d4a557e0f8fc5dc8ac8643dd7a2b9be4e2
Mule129 <a24cc410s@naver.com>
2023-08-18 11:03:51 +0900

    [Fix] #102 - fixed TodayWordCard undefind

--- src/pages/todayWordPage/TodayWordCard.jsx
+++ src/pages/todayWordPage/TodayWordCard.jsx
@@ -13,33 +13,14 @@ import '../../styles/todayWordPage/TodayWordCard.scss';
 /**
  *
  * @param {wordId: string, wordKr: string, wordEn: string, pronunciation: string, meaning: string,
- * ExampleSentence: {sentenceKr: string, sentenceEn: string}}} wordData wordData json data
+ * ExampleSentence: {sentenceKr: string, sentenceEn: string}} wordData wordData json data
  * @returns
  */
 function TodayWordCard({ wordData, index, onClick }) {
     let [isClickCard, setIsClickCard] = useState(false);
     let [RegisterFailedToast, onRegisterFailedToastCall] = ToastGenerator();
-    const [wordCard, setWordCard] = useState(
-        wordData === undefined
-            ? {
-                  id: 'word1',
-                  wordKr: '사과',
-                  wordEn: 'Apple',
-                  pronunciation: '[sagua]',
-                  meaning: 'noun-Apple',
-                  exampleSentence: [
-                      {
-                          sentenceKr: '나는 사과사의 아이폰을 사용한다.',
-                          sentenceEr: 'I am using iPhone by apple company',
-                      },
-                      {
-                          sentenceKr: '나는 사과사의 아이폰을 사용한다.',
-                          sentenceEr: 'I am using iPhone by apple company',
-                      },
-                  ],
-              }
-            : wordData[index],
-    );
+
+    const [wordCard, setWordCard] = useState(wordData ? null : wordData[index]);
 
     const onMySenteceClick = () => {
         onRegisterFailedToastCall();
@@ -63,51 +44,62 @@ function TodayWordCard({ wordData, index, onClick }) {
             <div className="wordInfo-frame">
                 <div className="mean">
                     <div className="text-box">
-                        <div className="krWord">{wordCard.wordKr}</div>
+                        <div className="krWord">
+                            {wordCard && wordCard.wordKr}
+                        </div>
                         {/* <div className="sound-box">
                             <img src={Sound} alt="sound" />
                         </div> */}
                         <div className="pronunciation">
-                            {wordCard.pronunciation}
+                            {wordCard && wordCard.pronunciation}
                         </div>
                     </div>
-                    <div className="todaywordcard-info">{wordCard.wordEn}</div>
+                    <div className="todaywordcard-info">
+                        {wordCard && wordCard.wordEn}
+                    </div>
                 </div>
                 <div className="bookmark-box">
-                    <Bookmark wordId={wordCard.id} />
+                    <Bookmark wordId={wordCard && wordCard.id} />
                 </div>
             </div>
             <div className="example-frame">
                 Example sentence
-                {Object.values(wordCard.exampleSentence).map((item, index) => (
-                    <ExampleSentence
-                        isSound={false}
-                        ExampleSentence={wordCard.exampleSentence[index]}
-                        key={index}
-                    />
-                ))}
+                {wordCard &&
+                    Object.values(wordCard.exampleSentence).map(
+                        (item, index) => (
+                            <ExampleSentence
+                                isSound={false}
+                                ExampleSentence={
+                                    wordCard.exampleSentence[index]
+                                }
+                                key={index}
+                            />
+                        ),
+                    )}
             </div>
-            <div className="option-frame">
-                <Link
-                    to={'./reply?id=' + wordCard.id}
-                    className="option-buttion link-offset-2 link-underline link-underline-opacity-0"
-                >
-                    <div className="icon-box">
-                        <img src={Reply} alt="Reply" />
-                    </div>
-                    Reply
-                </Link>
-                <Link
-                    to="."
-                    className="option-buttion link-offset-2 link-underline link-underline-opacity-0"
-                    onClick={onMySenteceClick}
-                >
-                    <div className="icon-box">
-                        <img src={MySentences} alt="My Sentences" />
-                    </div>
-                    My Sentences
-                </Link>
-            </div>
+            {wordCard && (
+                <div className="option-frame">
+                    <Link
+                        to={'./reply?id=' + wordCard.id}
+                        className="option-buttion link-offset-2 link-underline link-underline-opacity-0"
+                    >
+                        <div className="icon-box">
+                            <img src={Reply} alt="Reply" />
+                        </div>
+                        Reply
+                    </Link>
+                    <Link
+                        to="."
+                        className="option-buttion link-offset-2 link-underline link-underline-opacity-0"
+                        onClick={onMySenteceClick}
+                    >
+                        <div className="icon-box">
+                            <img src={MySentences} alt="My Sentences" />
+                        </div>
+                        My Sentences
+                    </Link>
+                </div>
+            )}
             <RegisterFailedToast
                 message="아직 준비중인 기능입니다"
                 icon={false}
